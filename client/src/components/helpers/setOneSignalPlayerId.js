581a6747bd1ae3b5c8332186fb9eadd432b49166
Yug sudani <yugsudani88@gmail.com>
2026-01-12 15:06:14 +0530

    Add files via upload

---
+++ client/src/components/helpers/setOneSignalPlayerId.js
@@ -0,0 +1,16 @@
+import api from "../../lib/api";
+
+async function setOneSignalPlayerId(playerId) {
+  // Send player ID to backend if user enabled notifications
+  if (!playerId) playerId = null;
+  console.log("playerId saved : ", playerId);
+  try {
+    await api.post("/user/saveOneSignalPlayerId", { playerId });
+    console.log("Player ID saved successfully");
+  } catch (err) {
+    console.error("Failed to save player ID:", err);
+    // Don't block login if saving player ID fails
+  }
+}
+
+export default setOneSignalPlayerId;
