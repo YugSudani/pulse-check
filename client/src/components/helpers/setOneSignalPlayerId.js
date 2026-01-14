9412a36d99e6052108d9bc7753157522243f23ca
YugSudani <yugsudani88@gmail.com>
2026-01-14 10:22:02 +0530

    fix / client for git merge

--- client/src/components/helpers/setOneSignalPlayerId.js
+++ client/src/components/helpers/setOneSignalPlayerId.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import api from "../../lib/api";
 
 async function setOneSignalPlayerId(playerId) {
@@ -15,21 +14,3 @@ async function setOneSignalPlayerId(playerId) {
 }
 
 export default setOneSignalPlayerId;
-=======
-import api from "../../lib/api";
-
-async function setOneSignalPlayerId(playerId) {
-  // Send player ID to backend if user enabled notifications
-  if (!playerId) playerId = null;
-  console.log("playerId saved : ", playerId);
-  try {
-    await api.post("/user/saveOneSignalPlayerId", { playerId });
-    console.log("Player ID saved successfully");
-  } catch (err) {
-    console.error("Failed to save player ID:", err);
-    // Don't block login if saving player ID fails
-  }
-}
-
-export default setOneSignalPlayerId;
->>>>>>> onesignal
