145cce019e3d62c51e6ee324c6e21d9759ae3d19
YugSudani <yugsudani88@gmail.com>
2026-01-18 17:56:38 +0530

    fix/ notification box dissmiss button removed

--- server/helpers/sendPushNotification.js
+++ server/helpers/sendPushNotification.js
@@ -49,13 +49,7 @@ module.exports.sendAlertNotification = async function sendAlertNotification(
           {
             id: "view",
             text: "View Details",
-            icon: "https://your-domain.com/view-icon.png", // Optional
             url: `https://pulse-check-5qky.onrender.com/monitor/${monitor._id}`,
-          },
-          {
-            id: "dismiss",
-            text: "Dismiss",
-            icon: "https://your-domain.com/close-icon.png", // Optional
           },
         ],
       },
